647f37c6ca22ee6abcddb1a6a6f78e4117b0f73a
Roberto Ross Leon <dev@rross.me>
2020-03-15 10:46:19 -0600

    All upload file routes working.

--- api/users/files.js
+++ api/users/files.js
@@ -5,10 +5,11 @@ const filesRouter = express.Router();
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'documentos/curp')
+    cb(null, 'documentos' + req.path)
   },
   filename: function (req, file, cb) {
     console.log(file);
+    console.log(req.path);
     type = file.mimetype.split('/');
     cb(null, 'curp-' + req.userId + '.pdf'); //file.fieldname + '-' + Date.now() + '.' + type[1]);
   }
@@ -25,7 +26,51 @@ filesRouter.post('/curp', upload.single('file'), (req, res, next) => {
 
   }
   res.send(file)
-})
+});
+
+filesRouter.post('/rfc', upload.single('file'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+
+  }
+  res.send(file)
+});
+
+filesRouter.post('/domicilio', upload.single('file'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+
+  }
+  res.send(file)
+});
+
+filesRouter.post('/profesional', upload.single('file'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+
+  }
+  res.send(file)
+});
+
+filesRouter.post('/pago', upload.single('file'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+
+  }
+  res.send(file)
+});
 /*
 const curpRouter = require('./files/curp');
 filesRouter.use('/curp', curpRouter);
