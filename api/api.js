beafd5c0a80ac7a07777d204fb858d8e7efc3cf1
Roberto Ross Leon <dev@rross.me>
2020-03-15 15:29:29 -0600

    Convocatories and places routes added. Need to add columns to both tables.

--- api/api.js
+++ api/api.js
@@ -5,10 +5,14 @@ const apiRouter = express.Router();
 const newsletterRouter = require('./newsletter/newsletter')
 const usersRouter = require('./users/users');
 const authRouter = require('./auth/auth');
+const convocatoriesRouter = require('./convocatories/convocatories');
+const placesRouter = require('./convocatories/places');
 //apiRouter.use('/imgs', usersRouter);
 
 apiRouter.use('/newsletter', newsletterRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/auth', authRouter);
+apiRouter.use('/convocatories', convocatoriesRouter);
+apiRouter.use('/places', placesRouter);
 
 module.exports = apiRouter;
